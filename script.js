37e9f2fa28b76f9078340947049529d77160f5f9
Eri Nagashima <es6.ukf@gmail.com>
2020-05-06 22:08:24 +0900

    Update

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ const picArray = [
 //スライドショーが再生中かどうかを記録しておくためのグローバル変数
 let playingID = 0;
 
-let changeAction = false; //ボタンクリック時の動作の切替
-
 //１番目の画像と画像タイトルを表示
 document.getElementById('pics').src = picArray[0].src;
 document.getElementById('pic-title').innerText = picArray[0].title;
@@ -20,36 +18,27 @@ document.getElementById('pic-title').innerText = picArray[0].title;
 //写真を切り替える関数
 //関数名「changePicture」
 let counter = 0;
+
 function changePicture() {
-  if (counter === 0) {
-    document.getElementById('pics').src = picArray[1].src;
-    document.getElementById('pic-title').innerText = picArray[1].title;
-  } else if (counter === 1) {
-    document.getElementById('pics').src = picArray[2].src;
-    document.getElementById('pic-title').innerText = picArray[2].title;
-  } else if (counter === 2) {
-    document.getElementById('pics').src = picArray[3].src;
-    document.getElementById('pic-title').innerText = picArray[3].title;
-  } else if (counter === 3) {
-    document.getElementById('pics').src = picArray[4].src;
-    document.getElementById('pic-title').innerText = picArray[4].title;
-  } else if (counter === 4) {
-    document.getElementById('pics').src = picArray[5].src;
-    document.getElementById('pic-title').innerText = picArray[5].title;
-  }
   counter++;
+
+  if (counter < picArray.length) {
+    document.getElementById('pics').src = picArray[counter].src;
+    document.getElementById('pic-title').innerText = picArray[counter].title;
+  }
+  // counter++; //ここに置くとpicture1が一回多く表示されるのはなぜ？
 }
 
 // ボタンを押してスライドショーを再生・停止する関数
 //関数名「playSlidedeshow」
 function playSlideshow() {
-  if (changeAction) {
+  if (playingID) {
+    // playingID = 0; //ここに置くとclearIntervalが作動しない、なぜか？
     clearInterval(playingID);
     document.getElementById('playButton').innerText = 'PLAY';
-    changeAction = false;
+    playingID = 0;
   } else {
     playingID = setInterval(changePicture, 2000);
     document.getElementById('playButton').innerText = 'STOP';
-    changeAction = true;
   }
 }
